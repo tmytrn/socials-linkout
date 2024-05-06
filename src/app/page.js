d8f7b97e414d2a68c05879143f89d6f222333420
Tommy Tran <tommy@tmytrn.com>
2024-05-06 12:27:09 -0400

    fixing link images

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-full flex-col justify-between items-center px-4 py-48">
+    <main className="flex h-full w-full flex-col justify-between items-center px-4 py-48">
       <div className="md:hidden z-10 flex flex-col w-full h-full items-center justify-between text-sm">
         <a href="https://instagram.com">
           <Image
-            src="/instagram.svg"
+            src="/public/instagram.svg"
             alt="instagram link"
             width={120}
             height={36}
@@ -15,7 +15,7 @@ export default function Home() {
         </a>
         <a href="https://facebook.com">
           <Image
-            src="/facebook.svg"
+            src="/public/facebook.svg"
             alt="facebook link"
             width={120}
             height={36}
